e4d41a8a75bc413482431ebbb9c44ad913c03003
Lucas Aguiar <luqkinhaas@gmail.com>
2023-09-09 00:28:32 -0300

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,16 @@
-import node from '@astrojs/node';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  adapter: vercel(),
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
   image: {
-    domains: ['astro.build'],
-  },
+    domains: ['astro.build']
+  }
 });
