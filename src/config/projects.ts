2b08982592e3ec7f9c148cce40a2c842f836860d
Lucas Aguiar <luqkinhaas@gmail.com>
2024-04-09 00:15:35 -0300

    fix: update projects

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -12,18 +12,7 @@ export const projects = {
       slug: 'observatorio-sebrae-startups',
       repository: 'https://observatorio.sebraestartups.com.br',
       contents: [
-        'Foi desenvolvida uma Landing Page incrível para a empresa Data Insight Lab, com o objetivo de tornar o site existente mais moderno e organizado. Utilizando Next.js com o App Router, TypeScript e Tailwind CSS, o projeto ganhou um toque especial. A implementação do Next.js garante um carregamento rápido das páginas, enquanto o uso do TypeScript evita erros e facilita a manutenção. Além disso, o Tailwind CSS permitiu criar um design responsivo e consistente de forma descomplicada. Como resultado, a nova Landing Page apresenta um visual moderno e clean, proporcionando uma navegação suave e agradável aos visitantes. Certamente, essa nova abordagem será apreciada por todos!',
-      ],
-    },
-    {
-      title: 'Data Insight Lab',
-      description: 'Landing Page moderna e clean para Data Insight Lab.',
-      tags: ['Next.js', 'TailwindCSS'],
-      image: '/images/data-insight-lab.webp',
-      slug: 'data-insight-lab',
-      repository: 'https://github.com/LucasAguiarr/data-insight-lab',
-      contents: [
-        'Foi desenvolvida uma Landing Page incrível para a empresa Data Insight Lab, com o objetivo de tornar o site existente mais moderno e organizado. Utilizando Next.js com o App Router, TypeScript e Tailwind CSS, o projeto ganhou um toque especial. A implementação do Next.js garante um carregamento rápido das páginas, enquanto o uso do TypeScript evita erros e facilita a manutenção. Além disso, o Tailwind CSS permitiu criar um design responsivo e consistente de forma descomplicada. Como resultado, a nova Landing Page apresenta um visual moderno e clean, proporcionando uma navegação suave e agradável aos visitantes. Certamente, essa nova abordagem será apreciada por todos!',
+        'Projeto desenvolvido em parceria com o Sebrae Santa Catarina.',
       ],
     },
   ],
